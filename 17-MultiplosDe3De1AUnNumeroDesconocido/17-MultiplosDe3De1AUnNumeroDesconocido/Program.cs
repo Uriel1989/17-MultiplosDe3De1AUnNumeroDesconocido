2f8dafe7a2595e416b90211dab80bcb0840d2f33
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:31:54 -0300

    Add files via upload

---
+++ 17-MultiplosDe3De1AUnNumeroDesconocido/17-MultiplosDe3De1AUnNumeroDesconocido/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _17_MultiplosDe3De1AUnNumeroDesconocido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, a;
+            a = 0;
+
+            Console.WriteLine("Ingrese un numero: ");
+
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+            {
+
+                if (i % 3 == 0)
+                {
+                    a = a + 1;
+                }
+            }
+            Console.WriteLine("La cantidad de multiplos de 3 es de: " + a);
+            Console.ReadKey();
+        }
+    }
+}
